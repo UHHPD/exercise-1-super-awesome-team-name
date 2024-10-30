b1496257855cabd6b8df5716382b0e3ff0ba7b73
csammoray <148360008+csammoray@users.noreply.github.com>
2024-10-30 15:01:59 +0000

    Final version ex1

--- stichprobe.cc
+++ stichprobe.cc
@@ -1,5 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+std::ifstream fin("datensumme.txt");
+
 double mean(int N){
   double sum = 0;
-  for(int i = 0; i == N; ++i)
-    sum += 
+  
+  int zahl = 0;
+  
+  
+  double mean = 0;
+  for(int i = 0; i <= N; i++){
+    fin >> zahl;
+    sum += zahl;
+  }
+//  std::cout << sum << std::endl;
+  mean = sum/N;
+  std::cout << mean << std::endl;
+  fin.close();
+return mean;
+
+}
+
+int main(){
+  int N = 234;
+  mean(N);
 }
